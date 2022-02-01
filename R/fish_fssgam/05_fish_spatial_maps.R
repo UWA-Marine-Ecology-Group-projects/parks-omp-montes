47021ba6ab59215425715dc802fec7cd4e33358b
claudehspencer <claudespencer111@gmail.com>
2022-02-01 09:45:00 +0800

    fish spatial predictions

--- R/fish_fssgam/05_fish_spatial_maps.R
+++ R/fish_fssgam/05_fish_spatial_maps.R
@@ -14,121 +14,132 @@ library(viridis)
 library(raster)
 library(patchwork)
 library(sf)
-library(cowplot)
+
+#read in fish prediction data
+spreddf <- readRDS("output/site_fish_predictions.rds")   
 
 # bring in spatial layers
+#National marine parks
 aumpa  <- st_read("data/spatial/shape/AustraliaNetworkMarineParks.shp")           # all aus mpas
-sw_mpa <- aumpa[aumpa$ResName %in% c("Abrolhos"), ]                             # just Abrolhos Aus MP
-ab_npz <- sw_mpa[sw_mpa$ZoneName == "National Park Zone", ]
-ab_npz$parkid <- c(1:3)
+nw_mpa <- aumpa[aumpa$ResName %in% c("Montebello"), ]                             # just Abrolhos Aus MP
+# mb_npz <- nw_mpa[nw_mpa$ZoneName == "National Park Zone", ]
+# ab_npz$parkid <- c(1:3)
+#State marine parks
+wampa  <- st_read("data/spatial/shape/WA_MPA_2018.shp")
+mb_mpa <- wampa[wampa$NAME %in% "Montebello Islands" ,]
+
 wgscrs <- CRS("+proj=longlat +datum=WGS84")
 sppcrs <- CRS("+proj=utm +zone=50 +south +datum=WGS84 +units=m +no_defs")     # crs for sp objects
-ab_npz <- st_transform(ab_npz, sppcrs)
+nw_mpa <- st_transform(nw_mpa, sppcrs)
 
+max(spreddf$x) #361746.1
+min(spreddf$x) #313956.1
+max(spreddf$y) #7772921
+min(spreddf$y) #7717571
+mb_mpa_crop <- st_crop(mb_mpa,xmin = 313956.1, ymin = 7717571, xmax = 361746.1, ymax = 7772921)
 
-# read in outputs from 'R/habitat_fish_model_predict.R'
-# preddf <- readRDS("output/broad_habitat_predictions.rds")
-spreddf <- readRDS("output/site_fish_predictions.rds")                       # site predictions only
-#spreddf$sitens <- ifelse(spreddf$y > 6940000, 1, 0)
-
+#make points higher than 250 equal to 250 - outliers in total abundance data from crazy tpi values
 spreddf$p_totabund <- ifelse(spreddf$p_totabund > 250,250,spreddf$p_totabund)
+
+#wa MPA colours
+mb_mpa$waname <- gsub("( \\().+(\\))", "", mb_mpa$ZONE_TYPE)
+mb_mpa$waname <- gsub(" [1-4]", "", mb_mpa$waname)
+
+mb_mpa$waname <- dplyr::recode(mb_mpa$waname, 
+                               "General Use" = "General Use Zone",
+                               # "MMA" = "Marine Management Area",
+                               # "Recreation Area" = "Recreation Zone",
+                               # "Conservation Area" = "Sanctuary Zone",
+                               "Special Purpose Zone (Shore Based Activities)" = 
+                                 "Special Purpose Zone\n(Shore Based Activities)")
+
+wampa_cols <- scale_color_manual(values = c("Sanctuary Zone" = "#7bbc63",      #changed to NPZ color as test from #bfd054
+                                           #"Marine Nature Reserve" = "#bfd054",
+                                           #"Conservation Area" = "#b3a63d",
+                                           # "Habitat Protection Zone" = "#fffbcc",# State MPA colours
+                                          # "Fish Habitat Protection Area" = "#fbff85",
+                                           # "National Park Zone" = "#a4d194",
+                                           "General Use Zone" = "#bddde1",
+                                           "Recreation Zone" = "#f4e952",
+                                           "Special Purpose Zone" = "#c5bcc9"
+                                           # "Special Purpose Zone\n(Shore Based Activities)" = "#ba3030"
+                                           # "Special Purpose Zone\n(Habitat Protection)" = "#f0ac41",
+                                           #"Reef Observation Area" = "#ddccff",
+                                           #"Marine Management Area" = "#b7cfe1"
+))
+
+wampa_fills <- scale_fill_manual(values = c("Sanctuary Zone" = "#bfd054",
+                                            #"Marine Nature Reserve" = "#bfd054",
+                                            #"Conservation Area" = "#b3a63d",
+                                            # "Habitat Protection Zone" = "#fffbcc",# State MPA colours
+                                            #"Fish Habitat Protection Area" = "#fbff85",
+                                            # "National Park Zone" = "#a4d194",
+                                            "General Use Zone" = "#bddde1",
+                                            "Recreation Zone" = "#f4e952",
+                                            "Special Purpose Zone" = "#c5bcc9"
+                                            # "Special Purpose Zone\n(Shore Based Activities)" = "#ba3030"
+                                            # "Special Purpose Zone\n(Habitat Protection)" = "#f0ac41",
+                                            #"Reef Observation Area" = "#ddccff",
+                                            #"Marine Management Area" = "#b7cfe1"
+))
 
 # plotting broad maps
 #total abundance
 p11 <- ggplot() +
   geom_tile(data = spreddf, aes(x, y, fill = p_totabund)) +
   scale_fill_viridis(direction = -1) +
-  #geom_sf(data = ab_npz[ab_npz$parkid == 2, ], fill = NA, colour = "#7bbc63") +
+  geom_sf(data = nw_mpa, fill = NA, colour = "#b9e6fb", size = 0.8) +
+  geom_sf(data = mb_mpa%>%dplyr::filter(waname%in%"Sanctuary Zone"), fill = NA, aes(color = waname), size = 0.8, show.legend = F) +
   theme_minimal() +
-  scale_x_continuous(breaks = c(113.2,113.4,113.6))+
-  labs(x = NULL, y = NULL, fill = "Total Abundance")+theme(plot.margin = unit(c(0, 0, 0, 0), "cm"))
+  wampa_cols+
+  coord_sf(xlim=c(313956.1,361746.1),ylim = c(7717571,7772921))+
+  #scale_x_continuous(breaks = c(113.2,113.4,113.6))+
+  labs(x = NULL, y = NULL, fill = "Total Abundance")#+theme(plot.margin = unit(c(0, 0, 0, 0), "cm"))
 p11
 
 #species richness
 p21 <- ggplot() +
-  geom_raster(data = spreddf[spreddf$sitens == 0, ], aes(x, y, fill = p_richness6)) +
+  geom_tile(data = spreddf, aes(x, y, fill = p_richness)) +
   scale_fill_viridis(direction = -1) +
-  geom_sf(data = ab_npz[ab_npz$parkid == 2, ], fill = NA, colour = "#7bbc63") +
+  geom_sf(data = nw_mpa, fill = NA, colour = "#b9e6fb", size = 0.8) +
+  geom_sf(data = mb_mpa%>%dplyr::filter(waname%in%"Sanctuary Zone"), fill = NA, aes(color = waname), size = 0.8, show.legend = F) +
   theme_minimal() +
-  scale_x_continuous(breaks = c(113.2,113.4,113.6))+
-  labs(x = NULL, y = NULL, fill = "Species Richness")+theme(plot.margin = unit(c(0, 0, 0, 0), "cm"))
+  wampa_cols+
+  coord_sf(xlim=c(313956.1,361746.1),ylim = c(7717571,7772921))+
+  #scale_x_continuous(breaks = c(113.2,113.4,113.6))+
+  labs(x = NULL, y = NULL, fill = "Species Richness")#+theme(plot.margin = unit(c(0, 0, 0, 0), "cm"))
 
 p21
 
 # greater than legal size
 p31 <- ggplot() +
-  geom_tile(data = spreddf[spreddf$sitens == 0, ], aes(x, y, fill = p_legal6)) +
+  geom_tile(data = spreddf, aes(x, y, fill = p_legal)) +
   scale_fill_viridis(direction = -1) +
-  geom_sf(data = ab_npz[ab_npz$parkid == 2, ], fill = NA, colour = "#7bbc63") +
+  geom_sf(data = nw_mpa, fill = NA, colour = "#b9e6fb", size = 0.8) +
+  geom_sf(data = mb_mpa%>%dplyr::filter(waname%in%"Sanctuary Zone"), fill = NA, aes(color = waname), size = 0.8, show.legend = F) +
   theme_minimal() +
-  scale_x_continuous(breaks = c(113.2,113.4,113.6))+
-  labs(x = NULL, y = NULL, fill = "Legal")+theme(plot.margin = unit(c(0, 0, 0, 0), "cm"))
+  wampa_cols+
+  coord_sf(xlim=c(313956.1,361746.1),ylim = c(7717571,7772921))+
+  #scale_x_continuous(breaks = c(113.2,113.4,113.6))+
+  labs(x = NULL, y = NULL, fill = "Legal")#+theme(plot.margin = unit(c(0, 0, 0, 0), "cm"))
 
 p31
 
 #smaller than legal size
 p41 <- ggplot() +
-  geom_tile(data = spreddf[spreddf$sitens == 0, ], aes(x, y, fill = p_sublegal6)) +
+  geom_tile(data = spreddf, aes(x, y, fill = p_sublegal)) +
   scale_fill_viridis(direction = -1) +
-  geom_sf(data = ab_npz[ab_npz$parkid == 2, ], fill = NA, colour = "#7bbc63") +
+  geom_sf(data = nw_mpa, fill = NA, colour = "#b9e6fb", size = 0.8) +
+  geom_sf(data = mb_mpa%>%dplyr::filter(waname%in%"Sanctuary Zone"), fill = NA, aes(color = waname), size = 0.8, show.legend = F) +
   theme_minimal() +
-  scale_x_continuous(breaks = c(113.2,113.4,113.6))+
-  labs(x = NULL, y = NULL, fill = "Sublegal")+theme(plot.margin = unit(c(0, 0, 0, 0), "cm"))
+  wampa_cols+
+  coord_sf(xlim=c(313956.1,361746.1),ylim = c(7717571,7772921))+
+  #scale_x_continuous(breaks = c(113.2,113.4,113.6))+
+  labs(x = NULL, y = NULL, fill = "Sublegal")#+theme(plot.margin = unit(c(0, 0, 0, 0), "cm"))
 
 p41
 
-#npz9
-#total abundance
-p1 <- ggplot() +
-  geom_tile(data = spreddf[spreddf$sitens == 1, ], aes(x, y, fill = p_totabund9)) +
-  scale_fill_viridis(direction = -1) +
-  geom_sf(data = ab_npz[ab_npz$parkid == 3, ], fill = NA, colour = "#7bbc63") +
-  theme_minimal() +
-  scale_x_continuous(breaks = c(113,113.10,113.2,113.3))+
-  labs(x = NULL, y = NULL, fill = "Total Abundance")+theme(plot.margin = unit(c(0, 0, 0, 0), "cm"))
-p1
+gg.predictions <- p11+p21+p31+p41 & theme(legend.justification = "left", aspect.ratio=1)
+gg.predictions
 
-#species richness
-p2 <- ggplot() +
-  geom_raster(data = spreddf[spreddf$sitens == 1, ], aes(x, y, fill = p_richness9)) +
-  scale_fill_viridis(direction = -1) +
-  geom_sf(data = ab_npz[ab_npz$parkid == 3, ], fill = NA, colour = "#7bbc63") +
-  theme_minimal() +
-  labs(x = NULL, y = NULL, fill = "Species Richness") +
-  scale_x_continuous(breaks = c(113,113.10,113.2,113.3))+theme(plot.margin = unit(c(0, 0, 0, 0), "cm"))
-
-p2
-
-# greater than legal size
-p3 <- ggplot() +
-  geom_tile(data = spreddf[spreddf$sitens == 1, ], aes(x, y, fill = p_legal9)) +
-  scale_fill_viridis(direction = -1) +
-  geom_sf(data = ab_npz[ab_npz$parkid == 3, ], fill = NA, colour = "#7bbc63") +
-  theme_minimal() +
-  labs(x = NULL, y = NULL, fill = "Legal") +
-  scale_x_continuous(breaks = c(113,113.10,113.2,113.3))+theme(plot.margin = unit(c(0, 0, 0, 0), "cm"))
-
-p3
-
-#smaller than legal size
-p4 <- ggplot() +
-  geom_tile(data = spreddf[spreddf$sitens == 1, ], aes(x, y, fill = p_sublegal9)) +
-  scale_fill_viridis(direction = -1) +
-  geom_sf(data = ab_npz[ab_npz$parkid == 3, ], fill = NA, colour = "#7bbc63") +
-  theme_minimal() +
-  labs(x = NULL, y = NULL, fill = "Sublegal") +
-  scale_x_continuous(breaks = c(113,113.10,113.2,113.3))+theme(plot.margin = unit(c(0, 0, 0, 0), "cm"))
-
-p4
-
-# gg.predictions.npz6 <- plot_grid(NULL,NULL,p11,p21,NULL,NULL,p31,p41,NULL,NULL,
-#                                  ncol = 2, align = "vh",rel_heights = c(-0.3,1,-0.5,1,0))
-
-gg.predictions.npz6 <- p11+p21+p31+p41 & theme(legend.justification = "left", aspect.ratio=1)
-gg.predictions.npz6
-
-gg.predictions.npz9 <- p1+p2+p3+p4 & theme(legend.justification = "left", aspect.ratio=1)
-gg.predictions.npz9
-
-ggsave("plots/site_fish_predictions-npz6.png", gg.predictions.npz6,width = 10, height = 7, dpi = 160)
-ggsave("plots/site_fish_predictions-npz9.png", gg.predictions.npz9,width = 10, height = 7, dpi = 160)
+ggsave("plots/site_fish_predictions.png", gg.predictions,width = 10, height = 8, dpi = 160)
